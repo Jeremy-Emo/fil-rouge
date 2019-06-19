0552ee123dce5875569aa5eb512c93f391c221b8
Moi <jeremy-emo@hotmail.fr>
2019-06-19 11:13:50 +0200

    menu admin

--- project/public/admin.py
+++ project/public/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from public.models import *
+from django.apps import apps
+
 
 # Register your models here.
+for model in apps.get_app_config('public').get_models():
+    try:
+        admin.site.register(model, eval(model.__name__ + "Admin") )
+    except:
+        admin.site.register(model, None)
+        pass
--- project/public/models.py
+++ project/public/models.py
@@ -6,8 +6,16 @@ class Favorites(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     movie_id = models.IntegerField()
 
+    class Meta:
+        verbose_name = 'Favori'
+        verbose_name_plural = 'Favoris'
+
 class Commentaries(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     note = models.IntegerField(default=5)
     movie_id = models.IntegerField(null=True)
+
+    class Meta:
+        verbose_name = 'Commentaire'
+        verbose_name_plural = 'Commentaires'
